cd732c5f56636bd44a36558c1f25e26393e10086
Shane Black <shane.black@hey.com>
2023-03-30 07:17:46 +1100

    Lab done 30/03

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(array, string) { 
+    return array.filter(function (drivers) {
+        return drivers === string || drivers === string.toLowerCase();
+    })
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter((element) => element.slice(0, string.length).toLowerCase().includes(string.toLowerCase()));
+}
+
+function matchName(array, string) {
+    return array.filter(driver => driver.name === string);
+}
